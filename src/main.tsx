fbd582115fd431ac83a3a4a583baff3c66454bae
Mathesh Mithra <matheshmithra@gmail.com>
2025-12-27 14:19:25 +0530

    Fix main.tsx for Cloudflare Pages (React + TS)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,16 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const root = document.getElementById("root");
+
+if (!root) {
+  throw new Error("Root container missing in index.html");
+}
+
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
